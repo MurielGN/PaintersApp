29a1a25d97b31a86098ebdd77b0af4bb3d4aea42
Muriel Gonzalez <78143766+MurielGN@users.noreply.github.com>
2022-03-31 10:29:45 +0200

    Update get-one-materials-painters.js

--- src/CRUD_lambda/get-one-materials-painters.js
+++ src/CRUD_lambda/get-one-materials-painters.js
@@ -2,39 +2,61 @@ const AWS = require("aws-sdk");
 
 const DYNAMO = new AWS.DynamoDB.DocumentClient();
 
-const TABLE_DYNAMODB = "paints";
+const TABLE_DYNAMODB = "materials";
+
+var statusCode;
 
 
-async function queryItems(filterStarWith){
+async function queryItems(id){
+  
   var params = {
     TableName: TABLE_DYNAMODB,
+    KeyConditionExpression: '#partitionKeyName = :partitionkeyval AND #sortKeyName = :sortkeyval',
+    ExpressionAttributeValues: { ':partitionkeyval': 'material', ':sortkeyval': id},
+    ExpressionAttributeNames: { '#partitionKeyName': 'element', '#sortKeyName': 'id'},
+  }
+  
+    let data = await DYNAMO.query(params).promise();
     
-    KeyConditionExpression: '#partitionKeyName = :partitionkeyval',
-    FilterExpression: 'begins_with (#starWith, :substr)',
-    ExpressionAttributeValues: { ':partitionkeyval': 'paint', ':substr': filterStarWith},
-    ExpressionAttributeNames: { '#partitionKeyName': 'element', '#starWith': 'type' },
-  }
+    if(data.Count == 0){
+      statusCode = 404;
+      throw new Error("Material with id: " + id + " not found");
+    }
+    
+    data = data.Items[0]
+    return data
   
-  try {
-    const data = await DYNAMO.query(params).promise()
-    return data.Items
-  } catch (err) {
-    return err
+}
+
+function verify(value) {
+  if (value <= 0 || isNaN(value)) {
+    statusCode = 400;
+    throw new Error("The id " + value + " is invalid");
   }
-  
 }
 
 
 exports.handler = async (event, context) => {
-  let body;
-  let statusCode = 200;
+  var body;
+  statusCode = 200;
 
-  let filterStarWith = event.expression;
-  body = await queryItems(filterStarWith);
-  
+  try {
+    
+    let id = event.id;
+    verify(id);
+    
+    body = await queryItems(id);
+    
     return {
-    statusCode,
-    body,
-  };
+      statusCode,
+      body
+    };
+  
+  } catch (err) {
+        return {
+      statusCode,
+      Error: err.toString(),
+    };
+  }
 
 }
