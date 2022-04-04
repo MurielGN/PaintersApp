c1a171a380e0772cbd5eb47188fc6fda50f710c8
Raul-HDEZ <raul.hdez.2001@outlook.es>
2022-04-04 11:12:11 +0200

    Deploy Updated

---
+++ deploy/CloudFormation/UploadToS3/materials/get-all-materials-painters/get-all-materials-painters.js
@@ -0,0 +1,34 @@
+const AWS = require("aws-sdk");
+
+const DYNAMO = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_DYNAMODB = "materials";
+
+async function queryItems() {
+  var params = {
+    TableName: TABLE_DYNAMODB,
+
+    KeyConditionExpression: "#partitionKeyName = :partitionkeyval",
+    ExpressionAttributeValues: { ":partitionkeyval": "material" },
+    ExpressionAttributeNames: { "#partitionKeyName": "element" },
+  };
+
+  try {
+    const data = await DYNAMO.query(params).promise();
+    return data.Items;
+  } catch (err) {
+    return err;
+  }
+}
+
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+
+  body = await queryItems();
+
+  return {
+    statusCode,
+    body,
+  };
+};
