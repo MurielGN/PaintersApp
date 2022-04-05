ea5acb4c8266be97097d0bbb703ad381283a4afa
MurielGN <muriel.glez@gmail.com>
2022-04-05 12:40:47 +0200

    finish serverless framework

---
+++ deploy/Serverless Framework/PaintersApp/src/CRUD_lambda/get-paints-StarWithName-painters.js
@@ -0,0 +1,62 @@
+const AWS = require("aws-sdk");
+
+const DYNAMO = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_DYNAMODB = "paints";
+
+var status;
+var msg;
+
+
+async function queryItems(expression){
+  var params = {
+    TableName: TABLE_DYNAMODB,
+    
+    KeyConditionExpression: '#partitionKeyName = :partitionkeyval',
+    FilterExpression: 'begins_with (#starWith, :substr)',
+    ExpressionAttributeValues: { ':partitionkeyval': 'paint', ':substr': expression},
+    ExpressionAttributeNames: { '#partitionKeyName': 'element', '#starWith': 'type' },
+  }
+  
+
+    const data = await DYNAMO.query(params).promise();
+    
+    if(data.Count == 0){
+      status = 404;
+      msg = `Paint name starting with: ${expression} not found`;
+      throw new Error(msg);
+    }
+    
+    return data.Items
+  
+}
+
+
+exports.handler = async (event, context) => {
+  let body;
+  status = 200;
+
+  try {
+    
+    let {expression} = event.pathParameters
+    if(typeof expression !== "string" || expression === ""){
+      status = 400;
+      msg = `Expression: ${expression} is invalid`;
+      throw new Error(msg);
+    }
+    
+    body = await queryItems(expression);
+    
+      return {
+      status,
+      body
+    };
+    
+  } catch (err) {
+      return {
+        status,
+        Error: err.toString(),
+    };
+  }
+  
+}
---
+++ deploy/Serverless Framework/PaintersApp/src/budget_lambda/budget-painters.js
@@ -0,0 +1,78 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+// LAMBDA ARREGLADA RAUL
+async function queryItems(tabla, valor, element) {
+    var params = {
+    TableName: tabla,
+    KeyConditionExpression:
+      "#partitionKeyName = :partitionkeyval AND #sortKeyName = :sortkeyval",
+    ExpressionAttributeValues: {
+      ":partitionkeyval": element,
+      ":sortkeyval": valor,
+    },
+    ExpressionAttributeNames: {
+      "#partitionKeyName": "element",
+      "#sortKeyName": "id",
+    },
+  };
+
+  const data = await dynamo.query(params).promise();
+  if (data.Items[0] == undefined) {
+    throw new Error("item with id: ${valor} not found ${data.Items)}");
+  } else {
+    return data.Items[0];
+  }
+}
+
+function verify(value) {
+  if (value <= 0 || isNaN(value)) {
+    throw new Error("The value ${value} is invalid");
+  }
+}
+
+exports.handler = async (event, context) => {
+  try {
+    
+    const statusCode = 200;
+    let data = event.item;
+    let budget = 0
+    
+    
+    for (let house of data) {
+      let tmpBudget = 0;
+      for (let material of house.materials) {
+        verify(material.amount)
+        
+        let materialDB = await queryItems("materials", material.id,"material");
+        tmpBudget += materialDB.price * material.amount;
+      }
+
+      for (let paint of house.paints) {
+        verify(paint.surface)
+        
+        let pinturaBD = await queryItems("paints", paint.id,"paint");
+        tmpBudget += (paint.surface / pinturaBD.efficiency) * pinturaBD.price;
+      }
+
+      tmpBudget *= house.numberofhouses;
+
+      tmpBudget *= 1 + house.benefits / 100;
+
+      budget += tmpBudget;
+
+      budget = Math.round(budget * 100) / 100;
+    }
+
+    return {
+      statusCode,
+      budget: budget,
+    };
+    
+  } catch (err) {
+    return {
+      statusCode: 405,
+      Error: err.toString(),
+    };
+  }
+};
