3bfff6489bf5836d361465bc74109444efbed9f7
Raul-HDEZ <raul.hdez.2001@outlook.es>
2022-04-06 10:22:03 +0200

    Script Update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 mkdir toUpload
+cp cloudformation-yaml/* toUpload
+echo "Nombre del bucket a crear?"
+read bucket
+
+aws s3 mb s3://$bucket
 
 for file in $(ls ./src)
 do
@@ -8,3 +13,7 @@ name=$(echo "$file" | cut -f 1 -d '.')
 echo "Zipping ${file}"
 zip toUpload/$name src/$file
 done
+
+aws s3 sync ./toUpload s3://$bucket
+
+rm -R toUpload
