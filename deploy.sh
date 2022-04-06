7848515cb0f6aae9782961fb98c2fe036ed314c1
MurielGN <muriel.glez@gmail.com>
2022-04-06 13:41:28 +0200

    tab deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,15 +21,16 @@ sed -i " ${linea2} s/.*/    Default: \'${role_arn}\' /" deployall-packaged.yaml
 aws s3 mb s3://$bucket
 
 for file in $(ls ./src)
-do
-name=$(echo "$file" | cut -f 1 -d '.')
-echo "Zipping ${file}"
-cd ./src
-mv $file index.js
-zip ../toUpload/$name index.js
-mv index.js $file
-cd ..
-done
+  do
+    name=$(echo "$file" | cut -f 1 -d '.')
+    echo "Zipping ${file}"
+    cd ./src
+    mv $file index.js
+    zip ../toUpload/$name index.js
+    mv index.js $file
+    cd ..
+  done
+
 aws s3 sync ./toUpload s3://$bucket
 
 rm -R toUpload
